471e39e9b8f506801a1a9da0f55768e284eeaba0
Nermeen-gh <ghazalnermeen1@gmail.com>
2025-09-20 18:40:22 +0300

    1st edition

---
+++ story_content/triggers.js
@@ -0,0 +1,71 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5Vtfg9Cskzj":
+        Script1();
+        break;
+      case "67NccxTpXx6":
+        Script2();
+        break;
+      case "5vknh5ZhDFr":
+        Script3();
+        break;
+      case "6LrcvTShiEL":
+        Script4();
+        break;
+      case "6RUWgtR45qi":
+        Script5();
+        break;
+      case "6Dv5wQsk9sE":
+        Script6();
+        break;
+      case "6TGnWeFkQnK":
+        Script7();
+        break;
+      case "6poMABDis5M":
+        Script8();
+        break;
+      case "5wj73c7bPFX":
+        Script9();
+        break;
+      case "5tPJhQY1Q0r":
+        Script10();
+        break;
+      case "5UoZdaA6E4I":
+        Script11();
+        break;
+      case "5jRzA4twYxL":
+        Script12();
+        break;
+      case "69lI8al1XoM":
+        Script13();
+        break;
+      case "6EIJ9YKHdS9":
+        Script14();
+        break;
+      case "5x16snnAsbh":
+        Script15();
+        break;
+      case "6BAf9UObdWL":
+        Script16();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
---
+++ story_content/user.js
@@ -0,0 +1,480 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script2 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script3 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script4 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script5 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script6 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script7 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script8 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script9 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script10 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script11 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script12 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script13 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script14 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script15 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+window.Script16 = function()
+{
+  // Storyline full-screen toggle
+(function() {
+  var docElm = document.documentElement;
+  
+  // If already in fullscreen, exit; otherwise, request
+  if (
+    document.fullscreenElement || 
+    document.webkitFullscreenElement || 
+    document.mozFullScreenElement || 
+    document.msFullscreenElement
+  ) {
+    // Exit full screen
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+  } else {
+    // Enter full screen
+    if (docElm.requestFullscreen) docElm.requestFullscreen();
+    else if (docElm.webkitRequestFullscreen) docElm.webkitRequestFullscreen();
+    else if (docElm.mozRequestFullScreen) docElm.mozRequestFullScreen();
+    else if (docElm.msRequestFullscreen) docElm.msRequestFullscreen();
+  }
+})();
+
+}
+
+};
